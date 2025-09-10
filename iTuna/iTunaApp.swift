ff31e2f41cbfa96864a14170d2db378d6adb9ac3
Rylie Castell <rylie.castell@gmail.com>
2025-09-10 21:21:57 +0200

    Initial Commit

---
+++ iTuna/iTunaApp.swift
@@ -0,0 +1,17 @@
+//
+//  iTunaApp.swift
+//  iTuna
+//
+//  Created by Rylie Castell on 10.09.25.
+//
+
+import SwiftUI
+
+@main
+struct iTunaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
